27d7672f1debab2646ffb6ef275d53b6d1690f71
bhargav <bhargav.gurramkonda@gmail.com>
2019-05-31 20:21:17 +0530

    some init msg

---
+++ speech2text.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+
+    print('Say Something!')
+    audio = r.listen(source)
+
+try:
+
+    print('Google Recognized that you said :\n' + r.recognize_google(audio))
+
+except:
+
+    pass
